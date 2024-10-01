55f0129d40aac5f3beda9ba0e71f009f77c0b7e1
Alex Johnson <alex.johnson@ramseysolutions.com>
2024-10-01 09:26:49 -0500

    add slf4j to pom

--- src/main/java/com/alexjohnson/spring_boot_starter/SpringBootStarterApplication.java
+++ src/main/java/com/alexjohnson/spring_boot_starter/SpringBootStarterApplication.java
@@ -1,13 +1,16 @@
 package com.alexjohnson.spring_boot_starter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootStarterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterApplication.class, args);
+		log.info("Hello Spring Boot!");
 	}
 
 }
