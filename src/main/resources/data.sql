af18e058f1c3d4ce0967efcef29dca7d9ad9fb43
Alex Johnson <alex.johnson@ramseysolutions.com>
2024-10-18 14:10:13 -0500

    fix: create table before adding data from sql script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
+CREATE TABLE countries (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
 INSERT INTO countries (id, name) VALUES (1, 'USA');
 INSERT INTO countries (id, name) VALUES (2, 'France');
 INSERT INTO countries (id, name) VALUES (3, 'Brazil');
