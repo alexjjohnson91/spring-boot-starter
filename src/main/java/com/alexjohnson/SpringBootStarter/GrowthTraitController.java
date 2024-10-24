7ef37aa971a5603e9ae2508804b56b0104ed21f3
Alex Johnson <alex.johnson@ramseysolutions.com>
2024-10-24 14:33:34 -0500

    refactor: change Id endpoint to request params

--- src/main/java/com/alexjohnson/SpringBootStarter/GrowthTraitController.java
+++ src/main/java/com/alexjohnson/SpringBootStarter/GrowthTraitController.java
@@ -4,7 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +18,8 @@ public class GrowthTraitController {
     return growthTraitRepository.findAll();
   }
 
-  @GetMapping("/growth-traits/{id}")
-  GrowthTrait one(@PathVariable Long id) {
+  @GetMapping(path = "/growth-traits", params = {"id"})
+  GrowthTrait one(@RequestParam(name = "id", required = true) Long id) {
     return growthTraitRepository.findById(id).orElseThrow(
         () -> new GrowthTraitIdNotFoundException(id));
   }
