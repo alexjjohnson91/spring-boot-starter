b1916aad94985cec1e03e26711ebf6c0d0d2aeea
Alex Johnson <alex.johnson@ramseysolutions.com>
2024-10-25 12:17:59 -0500

    style: formatting

--- src/main/java/com/alexjohnson/SpringBootStarter/GrowthTraitController.java
+++ src/main/java/com/alexjohnson/SpringBootStarter/GrowthTraitController.java
@@ -18,20 +18,20 @@ public class GrowthTraitController {
     return growthTraitRepository.findAll();
   }
 
-  @GetMapping(path = "/growth-traits", params = {"id"})
+  @GetMapping(path = "/growth-traits", params = { "id" })
   GrowthTrait one(@RequestParam(name = "id", required = true) Long id) {
     return growthTraitRepository.findById(id).orElseThrow(
         () -> new GrowthTraitIdNotFoundException(id));
   }
 
-  @GetMapping(path = "/growth-traits", params = {"status"})
+  @GetMapping(path = "/growth-traits", params = { "status" })
   List<GrowthTrait> getGrowthTraitsByStatus(
       @RequestParam(name = "status", required = true) String status) {
     return growthTraitRepository.findByStatus(status).orElseThrow(
         () -> new GrowthTraitStatusNotFoundException(status));
   }
 
-  @GetMapping(path = "/growth-traits", params = {"trait_type"})
+  @GetMapping(path = "/growth-traits", params = { "trait_type" })
   List<GrowthTrait> getGrowthTraitsByTraitType(
       @RequestParam(name = "trait_type", required = true) String trait_type) {
     return growthTraitRepository.findByTraitType(trait_type)
