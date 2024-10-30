8e1386cb82c3544831a2d783cf4a789fdf05568c
Alex Johnson <alex.johnson@ramseysolutions.com>
2024-10-30 07:27:08 -0500

    test: add context test

--- src/test/java/com/alexjohnson/SpringBootStarter/SpringBootStarterApplicationTests.java
+++ src/test/java/com/alexjohnson/SpringBootStarter/SpringBootStarterApplicationTests.java
@@ -1,13 +1,20 @@
 package com.alexjohnson.SpringBootStarter;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootStarterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private GrowthTraitController growthTraitController;
 
+  @Test
+  void contextLoads() throws Exception {
+    assertThat(growthTraitController).isNotNull();
+  }
 }
