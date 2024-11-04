0eff96cf2beddb83d3debb675f9fb7fa473ba922
Alex Johnson <alex.johnson@ramseysolutions.com>
2024-11-04 12:54:10 -0600

    fix: fix lsp warnings

--- src/test/java/com/alexjohnson/SpringBootStarter/SpringBootStarterApplicationTests.java
+++ src/test/java/com/alexjohnson/SpringBootStarter/SpringBootStarterApplicationTests.java
@@ -2,7 +2,6 @@ package com.alexjohnson.SpringBootStarter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootStarterApplicationTests {
 
-  @Autowired
-  private GrowthTraitController growthTraitController;
+  @Autowired private GrowthTraitController growthTraitController;
 
   @Test
   void contextLoads() throws Exception {
